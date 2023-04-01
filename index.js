c7e0fd7259a41ea905406e90e8212362a7df9527
woowooyong <woowooyong@gmail.com>
2023-04-01 14:29:45 +0800

    refactor: 把forEach改寫成其他方法與修正表單驗證

--- index.js
+++ index.js
@@ -1,51 +1,41 @@
 //form area
-const loginForm = document.querySelector(".loginForm");
-const signUpForm = document.querySelector(".signUpForm");
+const loginForm = document.querySelector('.loginForm');
+const signUpForm = document.querySelector('.signUpForm');
 
 //login input
-const loginEmail = document.querySelector(".loginEmail");
-const loginPassword = document.querySelector(".loginPassword");
+const loginEmail = document.querySelector('.loginEmail');
+const loginPassword = document.querySelector('.loginPassword');
 
 //sign up input
-const signUpEmail = document.querySelector(".signUpEmail");
-const signUpName = document.querySelector(".signUpname");
-const signUpPassword = document.querySelector(".signUpPassword");
-const checkPassword = document.querySelector(".checkPassword");
+const signUpEmail = document.querySelector('.signUpEmail');
+const signUpName = document.querySelector('.signUpname');
+const signUpPassword = document.querySelector('.signUpPassword');
+const checkPassword = document.querySelector('.checkPassword');
 
 //buttons
-const loginBtn = document.querySelector(".loginBtn");
-const signUpBtn = document.querySelector(".signUpBtn");
-const signUpPageBtn = document.querySelector(".signUpPageBtn");
-const loginPageBtn = document.querySelector(".loginPageBtn");
-
-//all inputs
-const inputs = document.querySelectorAll("input");
+const loginBtn = document.querySelector('.loginBtn');
+const signUpBtn = document.querySelector('.signUpBtn');
+const signUpPageBtn = document.querySelector('.signUpPageBtn');
+const loginPageBtn = document.querySelector('.loginPageBtn');
 
 //切換至註冊頁面
-signUpPageBtn.addEventListener("click", () => {
+signUpPageBtn.addEventListener('click', () => {
   resetErrMsg();
-  clearInput();
-  loginForm.style.display = "none";
-  signUpForm.style.display = "flex";
+  loginForm.reset();
+  loginForm.style.display = 'none';
+  signUpForm.style.display = 'flex';
 });
 
 //回到登入頁面
-loginPageBtn.addEventListener("click", () => {
+loginPageBtn.addEventListener('click', () => {
   resetErrMsg();
-  clearInput();
-  loginForm.style.display = "flex";
-  signUpForm.style.display = "none";
+  signUpForm.reset();
+  loginForm.style.display = 'flex';
+  signUpForm.style.display = 'none';
 });
 
-//切換頁面時清空輸入資料
-const clearInput = () => {
-  inputs.forEach((input) => {
-    input.value = "";
-  });
-};
-
 //API url
-const url = "https://todoo.5xcamp.us";
+const url = 'https://todoo.5xcamp.us';
 const obj = {
   user: {},
 };
@@ -59,156 +49,177 @@ const signUp = () => {
   axios
     .post(`${url}/users`, obj)
     .then((res) => {
-      alertify.notify(res.data.message, "success", 0.8);
+      alertify.notify(res.data.message, 'success', 0.8);
       setTimeout(() => {
         location.reload();
       }, 900);
     })
     .catch((err) => {
-      alertify.notify(err.response.data.message, "error", 2);
+      alertify.notify(err.response.data.message, 'error', 2);
     });
 };
 
-signUpBtn.addEventListener("click", () => {
+signUpBtn.addEventListener('click', () => {
   if (
-    signUpEmail.value == "" ||
-    signUpPassword.value == "" ||
-    signUpName == "" ||
-    checkPassword == ""
+    signUpEmail.value === '' ||
+    signUpPassword.value === '' ||
+    signUpName.value === '' ||
+    checkPassword.value === ''
   ) {
-    alertify.alert("錯誤訊息", "請輸入正確資料");
+    checkLoginForm();
+    alertify.alert('錯誤訊息', '請輸入正確資料');
   } else {
     checkSignUpForm();
-    signUp();
   }
 });
 
 //使用者登入
 const login = () => {
-  obj.user.email = loginEmail.value;
-  obj.user.password = loginPassword.value;
+  obj.user.email = loginEmail.value.trim();
+  obj.user.password = loginPassword.value.trim();
   axios
     .post(`${url}/users/sign_in`, obj)
     .then((res) => {
       token = res.headers.authorization;
       nickname = res.data.nickname;
-      localStorage.setItem("userToken", token);
-      localStorage.setItem("userNickname", nickname);
-      alertify.notify(res.data.message, "success", 0.8);
+      localStorage.setItem('userToken', token);
+      localStorage.setItem('userNickname', nickname);
+      alertify.notify(res.data.message, 'success', 0.8);
       setTimeout(() => {
         redirect();
       }, 1000);
     })
     .catch((err) => {
       console.log(err);
-      alertify.notify(err.response.data.message, "error", 2);
+      alertify.notify(err.response.data.message, 'error', 2);
     });
 };
 
-loginBtn.addEventListener("click", () => {
-  if (loginEmail.value == "" || loginPassword.value == "") {
-    alertify.alert("錯誤訊息", "請輸入正確資料");
+const handleLogin = () => {
+  if (loginEmail.value === '' || loginPassword.value === '') {
+    alertify.alert('錯誤訊息', '請輸入正確資料');
   } else {
     checkLoginForm();
-    login();
+  }
+};
+
+loginBtn.addEventListener('click', handleLogin);
+
+document.addEventListener('keydown', (event) => {
+  if (event.key === 'Enter') {
+    handleLogin();
   }
 });
 
 //導向todo頁面
 const redirect = () => {
-  if (localStorage.getItem("userToken")) {
-    location.href = "./todo.html";
+  if (localStorage.getItem('userToken')) {
+    location.href = './todo.html';
   } else {
-    console.log("no token");
+    console.log('no token');
   }
 };
 
-//表單驗證
-let constraints = {
-  loginEmail: {
-    presence: {
-      message: "^請輸入Email",
-    },
-    email: {
-      message: "^不符合Email格式",
-    },
-  },
-  loginPassword: {
-    presence: {
-      message: "^請輸入密碼",
-    },
-    length: {
-      minimum: 6,
-      message: "^密碼需大於6個字元",
-    },
-  },
+//validate.js 表單驗證
+
+//註冊資料驗證
+const signUpConstraints = {
   signUpEmail: {
     presence: {
-      message: "^請輸入Email",
+      message: '^請輸入Email',
     },
     email: {
-      message: "^不符合Email格式",
+      message: '^不符合Email格式',
     },
   },
   signUpPassword: {
     presence: {
-      message: "^請輸入密碼",
+      message: '^請輸入密碼',
     },
     length: {
       minimum: 6,
-      message: "^密碼需大於6個字元",
+      message: '^密碼需大於6個字元',
     },
   },
   nickname: {
     presence: {
-      message: "^請輸入暱稱",
+      message: '^請輸入暱稱',
     },
   },
   confirmPassword: {
     presence: {
-      message: "^請輸入正確密碼",
+      message: '^請輸入正確密碼',
     },
     equality: {
-      attribute: "signUpPassword",
-      message: "^密碼錯誤",
+      attribute: 'signUpPassword',
+      message: '^密碼錯誤',
     },
   },
 };
 
-//登入資料驗證
-const checkLoginForm = () => {
-  const errors = validate(loginForm, constraints);
-  resetErrMsg();
-  if (errors) {
-    setErrMsg(errors);
-  }
-  console.log(errors);
-};
-
-//註冊資料驗證
 const checkSignUpForm = () => {
-  const errors = validate(signUpForm, constraints);
+  const errors = validate(signUpForm, signUpConstraints);
   resetErrMsg();
   if (errors) {
     setErrMsg(errors);
+  } else {
+    signUp();
   }
-  console.log(errors);
 };
 
-//清除錯誤提示
-const resetErrMsg = () => {
-  const errMsgs = document.querySelectorAll("span");
-  errMsgs.forEach((err) => {
-    err.innerHTML = "";
-  });
+//登入資料驗證
+
+const loginConstraints = {
+  loginEmail: {
+    presence: {
+      message: '^請輸入Email',
+    },
+    email: {
+      message: '^不符合Email格式',
+    },
+  },
+  loginPassword: {
+    presence: {
+      message: '^請輸入密碼',
+    },
+    length: {
+      minimum: 6,
+      message: '^密碼需大於6個字元',
+    },
+  },
+};
+
+const checkLoginForm = () => {
+  const errors = validate(loginForm, loginConstraints);
+  resetErrMsg();
+  if (errors) {
+    setErrMsg(errors);
+  } else {
+    login();
+  }
 };
 
 //顯示錯誤提示
 const setErrMsg = (errors) => {
-  let errMsg;
+  // let errMsg;
   const nameList = Object.keys(errors);
-  nameList.forEach((name) => {
-    errMsg = document.querySelector(`.${name}-msg`);
+  // nameList.forEach((name) => {
+  //   errMsg = document.querySelector(`.${name}-msg`);
+  //   errMsg.innerText = errors[name];
+  // });
+  for (const name of nameList) {
+    const errMsg = document.querySelector(`.${name}-msg`);
     errMsg.innerText = errors[name];
-  });
+  }
+};
+
+//清除錯誤提示
+const resetErrMsg = () => {
+  const errMsgs = document.querySelectorAll('span');
+  // errMsgs.forEach((err) => {
+  //   err.innerHTML = '';
+  // });
+  for (const err of errMsgs) {
+    err.innerHTML = '';
+  }
 };
--- todo.js
+++ todo.js
@@ -1,43 +1,57 @@
-const add_input = document.querySelector(".add_input");
-const add_btn = document.querySelector(".add_btn");
-const listArea = document.querySelector(".list");
-const tab = document.querySelector(".tab");
-const todoTabs = document.querySelectorAll(".tabState");
-const clear_btn = document.querySelector(".clear_btn");
-const remainingArea = document.querySelector(".remaining");
-const title = document.querySelector(".title");
-const logoutBtn = document.querySelector(".logout");
-let tabStatus = "all";
+const add_input = document.querySelector('.add_input');
+const add_btn = document.querySelector('.add_btn');
+const listArea = document.querySelector('.list');
+const tab = document.querySelector('.tab');
+const todoTabs = document.querySelectorAll('.tabState');
+const clear_btn = document.querySelector('.clear_btn');
+const remainingArea = document.querySelector('.remaining');
+const title = document.querySelector('.title');
+const logoutBtn = document.querySelector('.logout');
+let tabStatus = 'all';
 
 //API url
-const url = "https://todoo.5xcamp.us";
-let nickname = "";
-let token = "";
+const url = 'https://todoo.5xcamp.us';
+let nickname = '';
+let token = '';
 let todos = [];
-token = localStorage.getItem("userToken");
+token = localStorage.getItem('userToken');
 const config = {
   headers: {
     Authorization: token,
   },
 };
-nickname = localStorage.getItem("userNickname");
+nickname = localStorage.getItem('userNickname');
 
 //渲染待辦清單
 const renderTodo = (todos) => {
-  let str = "";
-  todos.forEach((item) => {
-    str += `<li>
+  // let str = "";
+  // todos.forEach((item) => {
+  //   str += `<li>
+  //       <label for="" class="checkbox">
+  //         <input type="checkbox" data-id="${item.id}"
+  //         ${item.completed_at ? "checked" : ""}/>
+  //         <span>${item.content}</span>
+  //       </label>
+  //       <button class="delete_btn" data-id="${item.id}">
+  //         <i class="fa-solid fa-xmark" data-id="${item.id}"></i>
+  //       </button>
+  //     </li>`;
+  // });
+  // listArea.innerHTML = str;
+  const items = todos.map((item) => {
+    const { id, completed_at, content } = item;
+    return `<li>
         <label for="" class="checkbox">
-          <input type="checkbox" data-id="${item.id}" 
-          ${item.completed_at ? "checked" : ""}/>
-          <span>${item.content}</span>
+          <input type="checkbox" data-id="${id}" 
+          ${completed_at ? 'checked' : ''}/>
+          <span>${content}</span>
         </label>
-        <button class="delete_btn" data-id="${item.id}">
-          <i class="fa-solid fa-xmark" data-id="${item.id}"></i>
+        <button class="delete_btn" data-id="${id}">
+          <i class="fa-solid fa-xmark" data-id="${id}"></i>
         </button>
       </li>`;
   });
-  listArea.innerHTML = str;
+  listArea.innerHTML = items.join('');
 };
 
 //取得待辦清單
@@ -66,27 +80,23 @@ const addTodo = (todo) => {
       config
     )
     .then(() => {
-      alertify.notify("已新增", "success", 1);
+      alertify.notify('已新增', 'success', 1);
       getTodo();
     })
     .catch((err) => console.log(err.response));
 };
 
-add_btn.addEventListener("click", () => {
-  if (add_input.value.trim() == "") {
-    alertify.alert("錯誤訊息", "請先輸入資料");
+add_btn.addEventListener('click', () => {
+  if (add_input.value.trim() === '') {
+    alertify.alert('錯誤訊息', '請先輸入資料');
     return;
   }
   const newTodo = add_input.value;
   addTodo(newTodo);
   renderTodo(todos);
-  add_input.value = "";
+  add_input.value = '';
   //新增後回到'全部'頁籤
-  todoTabs.forEach((tab) => {
-    tab.classList.remove("active");
-  });
-  todoTabs[0].classList.add("active");
-  tabStatus = "all";
+  resetTodoTabs();
 });
 
 //刪除待辦事項
@@ -94,29 +104,29 @@ const deleteTodo = (todoId) => {
   axios
     .delete(`${url}/todos/${todoId}`, config)
     .then((res) => {
-      alertify.notify(res.data.message, "warning", 1);
+      alertify.notify(res.data.message, 'warning', 1);
       getTodo();
     })
     .catch((err) => console.log(err.response));
 };
 
-listArea.addEventListener("click", (e) => {
+listArea.addEventListener('click', (e) => {
   if (
-    e.target.getAttribute("class") == "delete_btn" ||
-    e.target.getAttribute("class") == "fa-solid fa-xmark"
+    e.target.getAttribute('class') === 'delete_btn' ||
+    e.target.getAttribute('class') === 'fa-solid fa-xmark'
   ) {
-    const todoId = e.target.getAttribute("data-id");
+    const todoId = e.target.getAttribute('data-id');
     deleteTodo(todoId);
   }
 });
 
 //切換待辦事項完成狀態
-listArea.addEventListener("click", (e) => {
+listArea.addEventListener('click', (e) => {
   if (
-    e.target.getAttribute("class") !== "delete_btn" &&
-    e.target.getAttribute("class") !== "fa-solid fa-xmark"
+    e.target.getAttribute('class') !== 'delete_btn' &&
+    e.target.getAttribute('class') !== 'fa-solid fa-xmark'
   ) {
-    const selectId = e.target.getAttribute("data-id");
+    const selectId = e.target.getAttribute('data-id');
     const selectTodo = todos.filter((todo) => {
       return todo.id === selectId;
     })[0];
@@ -129,53 +139,50 @@ listArea.addEventListener("click", (e) => {
   }
 });
 
-//清除已完成事項
-clear_btn.addEventListener("click", () => {
-  clearDoneItem();
-});
-
 const clearDoneItem = () => {
   if (todos.length > 0) {
     doneTodos = todos.filter((todo) => {
       return todo.completed_at !== null;
     });
     if (doneTodos.length > 0) {
-      alertify.notify("已清除所有完成事項", "warning", 1);
+      alertify.notify('已清除所有完成事項', 'warning', 1);
       axios.all(
         doneTodos.map((item) => {
           axios
             .delete(`${url}/todos/${item.id}`, config)
             .then((res) => {
-              doneTodos.forEach((item) => {
-                todos.splice(todos.indexOf(item), 1);
-              });
+              //因為使用axios.all刪除所有已完成事項後
+              //會再執行一次getTodo從server拿到新的todos
+              //就不需要再更新本地端的todos
+
+              // doneTodos.forEach((item) => {
+              //   todos.splice(todos.indexOf(item), 1);
+              // });
               getTodo();
             })
             .catch((err) => console.log(err.response));
         })
       );
       //清除後回到'全部‘頁籤
-      todoTabs.forEach((tab) => {
-        tab.classList.remove("active");
-      });
-      todoTabs[0].classList.add("active");
-      tabStatus = "all";
-
+      resetTodoTabs();
       setTodobyStatus(tabStatus);
     } else {
-      alertify.notify("目前沒有完成事項", "warning", 1);
+      alertify.notify('目前沒有完成事項', 'warning', 1);
     }
   } else {
-    alertify.notify("目前沒有待辦事項", "warning", 1);
+    alertify.notify('目前沒有待辦事項', 'warning', 1);
   }
 };
+
+//清除已完成事項
+clear_btn.addEventListener('click', clearDoneItem);
 
 //計算剩餘未完成事項
 const countUndone = (todos) => {
   const undoneNum = todos.filter((todo) => {
     return todo.completed_at === null;
   }).length;
-  if (undoneNum == 0) {
+  if (undoneNum === 0) {
     remainingArea.textContent = `沒事多喝水`;
     return;
   }
@@ -185,12 +192,12 @@ const countUndone = (todos) => {
 //redner todolist by status
 const setTodobyStatus = (status) => {
   switch (status) {
-    case "undone":
+    case 'undone':
       const undoneList = todos.filter((todo) => todo.completed_at === null);
       renderTodo(undoneList);
       break;
 
-    case "done":
+    case 'done':
       const doneList = todos.filter((todo) => todo.completed_at !== null);
       renderTodo(doneList);
       break;
@@ -201,20 +208,31 @@ const setTodobyStatus = (status) => {
 };
 
 //依選取狀態顯示待辦清單
-tab.addEventListener("click", (e) => {
-  if (e.target.nodeName === "UL") {
+tab.addEventListener('click', (e) => {
+  if (e.target.nodeName === 'UL') {
     return;
   }
   changeTab(e);
 });
 
+//重置Todo頁籤
+const resetTodoTabs = () => {
+  for (let tab of todoTabs) {
+    tab.classList.remove('active');
+  }
+  todoTabs[0].classList.add('active');
+  tabStatus = 'all';
+};
+
 const changeTab = (e) => {
   let selectedTab = e.target.dataset.state;
-  todoTabs.forEach((tab) => {
-    tab.classList.remove("active");
-  });
-
-  e.target.classList.add("active");
+  // todoTabs.forEach((tab) => {
+  //   tab.classList.remove('active');
+  // });
+  for (let tab of todoTabs) {
+    tab.classList.remove('active');
+  }
+  e.target.classList.add('active');
   tabStatus = selectedTab;
   setTodobyStatus(tabStatus);
   countUndone(todos);
@@ -225,24 +243,24 @@ const logout = () => {
   axios
     .delete(`${url}/users/sign_out`, config)
     .then((res) => {
-      localStorage.removeItem("userToken");
-      localStorage.removeItem("userNickname");
-      alertify.notify(res.data.message, "success", 0.8);
+      localStorage.removeItem('userToken');
+      localStorage.removeItem('userNickname');
+      alertify.notify(res.data.message, 'success', 0.8);
       setTimeout(() => {
         redirect();
       }, 900);
     })
     .catch((err) => {
       console.log(err);
-      alertify.alert("錯誤訊息", "登出失敗");
+      alertify.alert('錯誤訊息', '登出失敗');
     });
 };
 
-logoutBtn.addEventListener("click", logout);
+logoutBtn.addEventListener('click', logout);
 
 //初始化
 const init = () => {
-  if (!localStorage.getItem("userToken")) {
+  if (!localStorage.getItem('userToken')) {
     redirect();
   } else {
     title.innerText = `哈囉~ ${nickname} 來做點事吧!`;
@@ -251,7 +269,7 @@ const init = () => {
 };
 
 const redirect = () => {
-  location.href = "index.html";
+  location.href = 'index.html';
 };
 
 init();
